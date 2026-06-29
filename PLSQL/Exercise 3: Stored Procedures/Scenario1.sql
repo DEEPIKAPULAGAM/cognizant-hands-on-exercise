499f03f439ddcddc276177ce131de678384a0d35
DEEPIKAPULAGAM <148576757+DEEPIKAPULAGAM@users.noreply.github.com>
2026-06-29 20:01:55 +0530

    Create Scenario1.sql

---
+++ PLSQL/Exercise 3: Stored Procedures/Scenario1.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest
+IS
+BEGIN
+
+    UPDATE SavingsAccounts
+    SET Balance = Balance + (Balance * 0.01);
+
+    COMMIT;
+
+END;
+/
+
+EXEC ProcessMonthlyInterest;
+
+SELECT * FROM SavingsAccounts;
