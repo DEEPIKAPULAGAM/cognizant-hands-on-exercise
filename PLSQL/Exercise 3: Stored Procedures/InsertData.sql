67f788b73255f4e5177cb70a442e6aef1aa55746
DEEPIKAPULAGAM <148576757+DEEPIKAPULAGAM@users.noreply.github.com>
2026-06-29 20:00:09 +0530

    Create InsertData.sql

---
+++ PLSQL/Exercise 3: Stored Procedures/InsertData.sql
@@ -0,0 +1,25 @@
+INSERT INTO SavingsAccounts VALUES (101,'Rahul',10000);
+INSERT INTO SavingsAccounts VALUES (102,'Priya',20000);
+INSERT INTO SavingsAccounts VALUES (103,'Arjun',15000);
+
+COMMIT;
+
+
+-----------------------------------------------------------
+
+
+INSERT INTO Employees VALUES (1,'Amit','IT',50000);
+INSERT INTO Employees VALUES (2,'Neha','HR',45000);
+INSERT INTO Employees VALUES (3,'Ravi','IT',60000);
+
+COMMIT;
+
+
+-------------------------------------------------------------
+
+
+INSERT INTO Account VALUES (101,'Rahul',10000);
+INSERT INTO Account VALUES (102,'Priya',5000);
+INSERT INTO Account VALUES (103,'Arjun',2000);
+
+COMMIT;
